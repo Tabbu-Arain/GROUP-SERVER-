bd9bf9061e868d356d7b31fc3921c40c227d751b
Tabbu Arain <tbsmarain@gmail.com>
2025-02-11 23:42:22 +0500

    main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def send_message():
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>&#xf1fc; 𝙏𝘼𝘽𝘽𝙐 𝙓𝙒𝘿 😎</title>
+  <title>𝙏𝘼𝘽𝘽𝙐 𝙓𝘿 😎</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <style>
